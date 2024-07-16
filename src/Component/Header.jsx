4b9c737215eac049582ffcbb453f414926879efb
Ratul Islam <iratul010@gmail.com>
2024-07-16 18:15:48 +0600

    media-queries-start

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,22 @@
-
 const Header = () => {
   return (
     <div className="header">
-      Header
+      <img
+        src="/src/assets/img/logo.png"
+        alt="Nexter Logo"
+        className="header__logo"
+      />
+      <h3 className="heading-3">Your own home</h3>
+      <h1 className="heading-1">The ultimate personal freedom</h1>
+      <button className="btn header__btn"> View our properties</button>
+      <div className="header__seenon-text">As seen on</div>
+      <div className="header__seenon-logos">
+        <img src="/src/assets/img/logo-bbc.png" alt="Seenon logo1" />
+
+        <img src="/src/assets/img/logo-forbes.png" alt="Seenon logo 2" />
+        <img src="/src/assets/img/logo-techcrunch.png" alt="Seenon logo 3" />
+        <img src="/src/assets/img/logo-bi.png" alt="Seenon logo 4" />
+      </div>
     </div>
   );
 };
--- src/Component/Realtors.jsx
+++ src/Component/Realtors.jsx
@@ -2,7 +2,30 @@
 const Realtors = () => {
   return (
     <div className="realtors">
-    3 Realtors
+     <h3 className="heading-3">Top 3 Realtors</h3>
+     <div className="realtors__list">
+      <img src="/src/assets/img/realtor-1.jpeg" alt="Realtor 1" className="realtors__img"/>
+      <div className="realtors__details">
+        <h4 className="heading-4 heading-4--light">
+          Erik Feiman
+        </h4>
+        <p className="realtors__sold">245 houses sold</p>
+      </div>
+      <img src="/src/assets/img/realtor-2.jpeg" alt="Realtor 2" className="realtors__img"/>
+      <div className="realtors__details">
+        <h4 className="heading-4 heading-4--light">
+          Kim Brown
+        </h4>
+        <p className="realtors__sold">212 houses sold</p>
+      </div>
+      <img src="/src/assets/img/realtor-3.jpeg" alt="Realtor 3" className="realtors__img"/>
+      <div className="realtors__details">
+        <h4 className="heading-4 heading-4--light">
+          Toby Remsy
+        </h4>
+        <p className="realtors__sold">255 houses sold</p>
+      </div>
+     </div>
     </div>
   );
 };
